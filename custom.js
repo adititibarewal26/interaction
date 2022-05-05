91be09656a8b4348f445ca24f198c25774ead3e6
aditi <aditi.tibarewal_ug22@ashoka.edu.in>
2022-05-05 16:41:00 +0530

    ok

--- custom.js
+++ custom.js
@@ -16,10 +16,7 @@ const map = new mapboxgl.Map({
 map.on('load', () => {
   //Hide all presentation layers
   //This demo uses three specific layers. I want to hide them initially so I can reveal them piece meal.
-  map.setLayoutProperty('india', 'visibility', 'none');
-  map.setLayoutProperty('bang-pak', 'visibility', 'none');
-  map.setLayoutProperty('choropleth-2', 'visibility', 'none');
-
+  
   //to reduce clutter, the steps for creating a legend, slider, and menu have all been turned into functions.
   createLegend()
 
